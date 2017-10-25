0a0d061cda48f101016ee362c23d412613aa5b79
VanessaY <yu100997@gmail.com>
2017-10-25 18:08:42 -0400

    User/Password input

--- OurFO.java
+++ OurFO.java
@@ -2,6 +2,8 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import java.util.Scanner;
+
 public class OurFO {
 
 	public static void main(String[] argv) {
@@ -14,11 +16,24 @@ public class OurFO {
 			return;
 		}
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Postgres Username (Blank for \"postgres\":");
+        String username = sc.nextLine();
+        if (username.equals("")){
+            username = "postgres";
+        }
+        System.out.print("Postgres Username (Blank for \"CSCI320\":");
+        String password = sc.nextLine();
+        if (password.equals("")){
+            password = "CSCI320";
+        }
+
+
 		Connection connection= null;
 		try {
 			connection = DriverManager.getConnection(
-					"jdbc:postgresql://127.0.0.1:5432/ourfo", "postgres",
-					"CSCI320");
+					"jdbc:postgresql://127.0.0.1:5432/ourfo", username,
+					password);
 		} catch (SQLException e) {
 			System.out.println("Connection Failed! Check output console");
 			e.printStackTrace();
